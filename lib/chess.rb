00379071b3432532347e3a3cbf47608675b5864d
Christian Sanders <christian.sanders@live.com>
2018-08-12 13:54:18 -0700

    Add pseudo code

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,9 +1,29 @@
 class Chess
 =begin
   On new,
-    create board - editable
+    create board display - editable
     create pieces on board - editable
     the colors of the pieces - only readable
     and the position of the pieces - editable
+
+  On check color,
+    check for color
+    if not color, return false
+
+  On main menu,
+    displays main menu
+    gets user input
+    if user input is 1 sends new game
+    if user input is 2 sends load game
+
+  On new game,
+    blah blah blah
+
+  On load game,
+    blah blah blah
+
+  On save game,
+    blah blah blah
+
 =end
 end
---
+++ lib/pieces.rb
@@ -0,0 +1,17 @@
+class Piece
+=begin
+  On new,
+    takes position and color
+    x coordinate = first val of position - editable
+    y coordinate = second val of position - editable
+
+  On legal moves,
+    takes x and y
+    depending on color and position on board,
+    checks legal moves
+    if move is legal,
+    pushes move into an array
+    if move is not legal,
+    raises an error
+=end
+end
