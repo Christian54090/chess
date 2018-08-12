7bf9db91e08e3443c3b020531c96c6673c1c42a9
Christian Sanders <christian.sanders@live.com>
2018-08-12 12:08:57 -0700

    Initialize repo

---
+++ lib/chess.rb
@@ -0,0 +1,9 @@
+class Chess
+=begin
+  On new,
+    create board - editable
+    create pieces on board - editable
+    the colors of the pieces - only readable
+    and the position of the pieces - editable
+=end
+end
